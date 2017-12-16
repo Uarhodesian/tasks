54487508c4d8cc489b596d959794921e851f5d8f
Oleksii Danylyuk <danilyuk@bigmir.net>
2017-12-16 15:18:38 +0200

    caesar cipher

--- ex06.py
+++ ex06.py
@@ -1,11 +1,10 @@
 # 6.Define a function caesar_cipher that takes string and key(number), whuch returns encrypted string
-
-def caesar(st, key):
+def caesar(string, key):
     alphabet = 'abcdefghijklmnopqrstuvwxyz'
-    cipher = ""
-        for c in st:
-            if c in alphabet:
-                cipher += alphabet[(alphabet.index(c) + key) % len(alphabet)]
-                return cipher
+    cipher = " "
+    for i in string:
+            if i in alphabet:
+                    cipher += alphabet[(alphabet.index(i) + key) % len(alphabet)]
+                    return cipher
 
 
