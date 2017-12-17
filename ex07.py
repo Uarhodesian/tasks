1454c43e34d8929f15ed0583cfeadc2bb3b5dda9
Oleksii Danylyuk <danilyuk@bigmir.net>
2017-12-17 11:59:25 +0200

    matrix rotate

--- ex07.py
+++ ex07.py
@@ -2,13 +2,11 @@
 #1 2 3         1 4 7
 #4 5 6 returns 2 5 8
 #7 8 9         3 6 9
-#matrix = [[1,2,3],[4,5,6],[7,8,9]]
+#m = [[1,2,3],[4,5,6],[7,8,9]]
 
 def rotate(m):
     rm = []
     for i in range(0,len(m)):
             rm.append([row[i] for row in m])           
-    #return rm 
-#[[7, 4, 1], [8, 5, 2], [8, 6, 3]]
-    for c in rm:
-            print(rm)
+    return rm 
+#rm = [[1, 4, 7], [2, 5, 8], [3, 6, 9]]
