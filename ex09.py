f516c09994525e9b0b8bc43de3338d88213e3c46
Oleksii Danylyuk <danilyuk@bigmir.net>
2017-12-17 16:33:16 +0200

    brackets

---
+++ ex09.py
@@ -0,0 +1,11 @@
+# 9.Define a function, which takes a string with N opening brackets ("[") and N closing brackets ("]"), in some arbitrary order.
+#   Determine whether the generated string is balanced; that is, whether it consists entirely of pairs of opening/closing brackets (in that order), none of which mis-nest.
+#   Examples:
+#   []        OK   ][        NOT OK
+#   [][]      OK   ][][      NOT OK
+#   [[][]]    OK   []][[]    NOT OK
+
+def brackets():
+    you1 = input("Enter random string with opening/closing brackets [ or ] : ")
+    print("Your string", you1)
+   
