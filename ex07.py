57906d793c00dd32ae80c45b79afce5b34c31d1a
Oleksii Danylyuk <danilyuk@bigmir.net>
2017-12-17 09:50:31 +0200

    rotate matrix

---
+++ ex07.py
@@ -0,0 +1,11 @@
+# 7.define a function diaginal_reverse() that takes matrix and returns diagonal-reversed one:
+#1 2 3         1 4 7
+#4 5 6 returns 2 5 8
+#7 8 9         3 6 9
+#matrix = [[1,2,3],[4,5,6],[7,8,9]]
+
+def rotate(m):
+    rm = []
+    for i in range(0,len(m)):
+            rm.append([row[i] for row in reversed(m)])
+    return rm
