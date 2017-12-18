87f476695e9232cc5aa12c76765f8c7e7fdedd5f
Oleksii Danylyuk <danilyuk@bigmir.net>
2017-12-18 13:12:24 +0200

    brackets 2

--- ex09.py
+++ ex09.py
@@ -5,7 +5,21 @@
 #   [][]      OK   ][][      NOT OK
 #   [[][]]    OK   []][[]    NOT OK
 
+import re
+
+
 def brackets():
-    you1 = input("Enter random string with opening/closing brackets [ or ] : ")
-    print("Your string", you1)
-   
+    bracket_string = input("Enter random string with opening/closing brackets [ or ] : ")
+    print("Your string", bracket_string)
+    result = 0
+    # Remove all found pairs of brackets using regex
+    while len(re.findall(r'\[\]', bracket_string)) > 0:
+        result = re.sub(r'\[\]', '', bracket_string)
+
+        # If after removing all pairs of brackets the string is still not empty
+        if len(result) > 0:
+            print (bracket_string, 'NOT OK')
+            break
+        else:
+            print (bracket_string, 'OK')
+            break
