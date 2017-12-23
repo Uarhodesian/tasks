aafcd89c4db35e8a13630de49b7ccead82760a80
Oleksii Danylyuk <danilyuk@bigmir.net>
2017-12-23 13:21:50 +0200

    fibonacci 5

--- fibonacci.py
+++ fibonacci.py
@@ -1,7 +1,4 @@
-# 12. Write a ship battle game, which is similar to ex.8, except it takes 1 integer as an order of matrix,
-#     randomly generates index (x, y) and checks user input (2 integers).
-#     (tip: use var1, var2 = raw_input("Enter two numbers here: ").split())
-#     *Visualize the game.
+# Fibonacci sequence up to n-th term using recursive functions
 
 def recur_fibo(n):
    """Recursive function to
